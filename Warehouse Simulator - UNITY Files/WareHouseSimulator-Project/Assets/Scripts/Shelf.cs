dabb30efaab8dbbfef263194df0c9713581f5e9d
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-11-09 23:34:27 -0500

    Signs, some UI, and Shelf Logic
    -Added a Overhead Sign Model
    -Added UI
    -Worked on Shelf ID codes
    -Had to move Shelf Trigger onto the object itself
    -Discovered Issue with traveling between shelfs

--- Warehouse Simulator - UNITY Files/WareHouseSimulator-Project/Assets/Scripts/Shelf.cs
+++ Warehouse Simulator - UNITY Files/WareHouseSimulator-Project/Assets/Scripts/Shelf.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Shelf : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+    /// <summary>
+    /// The code for this type of pallet.
+    /// </summary>
+    public string areaCode;
+    /// <summary>
+    /// The letter representing what lane this shelf is in.
+    /// </summary>
+    public string laneLetter;
+    /// <summary>
+    /// The number shelf this is relative to the lane it's in.
+    /// </summary>
+    public int shelfNumber;
+
+    /// <summary>
+    /// The identification string of this shelf.
+    /// </summary>
+    public string ShelfID { get { return $"{areaCode.ToUpper()}:{laneLetter.ToUpper()}{shelfNumber}"; } }
+
+    /// <summary>
+    /// The User Interface in the scene.
+    /// </summary>
+    private UserInterface ui;
+    #endregion
+
+    private void Start()
     {
-        
+        name = $"{name.Substring(0, 11)}_{ShelfID}";
+
+        ui = FindObjectOfType<UserInterface>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //TODO: Don't want this as Stay, but collider is so big, it exit's while in the middle of another shelf
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            ui.DisplayShelfID(ShelfID);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ui.DisplayShelfID(string.Empty);
+        }
     }
 }
---
+++ Warehouse Simulator - UNITY Files/WareHouseSimulator-Project/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    #region Variables
+    /// <summary>
+    /// The Text that displays the currently visited Shelf ID.
+    /// </summary>
+    public Text shelfIDText;
+    #endregion
+
+    private void Start()
+    {
+        shelfIDText.text = string.Empty;
+    }
+
+    /// <summary>
+    /// Displays the currently visited Shelf ID.
+    /// </summary>
+    /// <param name="shelfID">
+    /// The shelf ID.
+    /// </param>
+    public void DisplayShelfID(string shelfID)
+    {
+        shelfIDText.text = shelfID;
+    }
+}
